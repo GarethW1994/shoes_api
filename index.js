27c7b4123b13757fdaa5b9bb35a23fcd2d67aea0
GarethW1994 <williams.gareth0@gmail.com>
2017-08-01 14:54:31 +0200

    added new routes

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 //require modules
 const express = require('express');
-
 //create express app
 var app = express();
 
 //require shoes.js function
 var Shoes = require('./shoes.js');
 
-//create root route
-app.get('/', Shoes().index);
+///////////////////GET ROUTES///////////////////////
+
+//show all the shoes route
+app.get('/api/shoes', Shoes().index);
+
+//list all the shoes for a given brand route
+app.get('/api/shoes/brand/:brandname', Shoes().brands);
+
+//list all shoes for a given size route
+app.get('/api/shoes/size/:size', Shoes().sizes);
+
+//list all shoes for a given brand and size route
+app.get('/api/shoes/brand/:brandname/size/:size', Shoes().sizeBrand);
 
 
-//create add shoe route
-app.get('/addShoe', Shoes().addShoe);
+///////////////////POST ROUTES///////////////////////
+
+//Update the stock levels when a shoe is sold
+app.post('/api/shoes/sold/:id', Shoes().soldUpdate);
+
+//Add a new shoe route
+app.post('/api/shoes', Shoes().addShoe);
 
 
 //create port
--- shoes.js
+++ shoes.js
@@ -1,18 +1,47 @@
 module.exports = function() {
+  //create shoes object
 
-  //create home route
+  //show all the shoes route
   var index = function(req, res) {
-    res.send('Shoe Catalogue API - Home Page');
+    //display all the shoes
+    res.send('Shoe Catalogue API - All The Shoes');
   };
 
-  //create add shoe route
+  //list all the shoes for a given brand route
+  var brands = function(req, res) {
+    res.send('Shoe Catalogue API - Brands Filter');
+  }
+
+  //list all shoes for a given size route
+  var sizes = function(req, res) {
+    res.send('Shoe Catalogue API - Sizes Filter');
+  }
+
+  //list all shoes for a given brand and size route
+  var sizeBrand = function(req, res) {
+    res.send('Shoe Catalogue API - Size and Brand Filter');
+  }
+
+  //Update the stock levels when a shoe is sold
+  var soldUpdate = function(req, res) {
+    res.send('Shoe Catalogue API - Update Sold Shoe');
+  }
+
+  //Add a new shoe route
   var addShoe = function(req, res) {
+    //add a new shoe
+
+    //redirect to the home route
     res.send('Shoe Catalogue API - Add Shoe Page');
   }
 
   //return routes
   return {
     index,
+    brands,
+    sizes,
+    sizeBrand,
+    soldUpdate,
     addShoe
   }
 }
