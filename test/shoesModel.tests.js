fd2f2b4c8c373151dea3ec485c9000caae71bb98
GarethW1994 <williams.gareth0@gmail.com>
2017-08-02 10:19:53 +0200

    added mocha test file

---
+++ test/shoesModel.tests.js
@@ -0,0 +1,8 @@
+//require assert
+const assert = require('assert');
+
+describe('store shoes', function() {
+    it('should add new shoes to MongoDB', function() {
+    //  assert.equal(1,2);
+    });
+});
