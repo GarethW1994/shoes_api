058e2ca9f2dfd230a597e80dc34cc06e74ef9e7a
GarethW1994 <williams.gareth0@gmail.com>
2017-08-04 13:32:30 +0200

    added json responses

--- api/routes/shoesRoutes.js
+++ api/routes/shoesRoutes.js
@@ -7,68 +7,109 @@ module.exports = function(Models) {
 
     //show all the shoes route
     const index = function(req, res, next) {
-          Models.Shoes.find({}, function(err, shoes) {
+          Models.Shoes.find({}, function(err, results) {
               if (err) {
-                return next(err);
-              }
-              res.send(shoes);
+                res.json({
+                  response: 'Status Failure',
+                  error: err,
+                  status: 500,
+                  data: results
+              });
+            }
+            //send all data to the client
+              res.json({
+                response: 'Got a GET request for all shoe data',
+                status: 200,
+                data: results
+              })
           });
     };
 
     //list all the shoes for a given brand route
     var brands = function(req, res, next) {
-      let shoe = {
-          brand: req.params.brandname
-        }
-
+      //save the
+        let query = {brand: req.params.brand};
+      //find the shoe in the database
         Models.Shoes.find(shoe, function(err, results){
           if (err) {
-            return next(err);
-          }
-          res.send(results);
+            res.json({
+              response: 'Status Failure',
+              error: err,
+              status: 500,
+              data: results
+          });
+        }
+        //send results to client
+          res.json({
+            response: 'Got a GET request for shoe brands',
+            status: 200,
+            data: results});
         });
     };
 
     //list all shoes for a given size route
     var sizes = function(req, res) {
-        let shoe = {
-          size: req.params.size
-        }
-
-        Models.Shoes.find(shoe, function(err, results){
+        //save the brand size in object
+        let query = {size: req.params.size};
+        //find the shoe in the database
+        Models.Shoes.find(query, function(err, results){
           if (err) {
+            res.json({
+              response: 'Status Failure',
+              error: err,
+              status: 500,
+              data: results
+            });
             return next(err);
           }
-          res.send(results);
+          res.json({
+            response: "Got a GET request for shoe sizes",
+            status: 200,
+            data: results
+          })
         });
     }
 
     //list all shoes for a given brand and size route
     var sizeBrand = function(req, res) {
-        let shoe = {
-          brand: req.params.brandname,
-          size: req.params.size
-        }
-
-        Models.Shoes.find(shoe, function(err, results){
-          if (err) {
-            return next(err);
-          }
-          res.send(results);
-        })
+          //save the brand name and shoe size in object
+          let query = {brand: req.params.brandname, size: req.params.size};
+          //find the shoe in the database
+          Models.Shoes.find(query, function(err, results){
+            if (err) {
+              res.json({
+                response: 'Status Failure',
+                error: err,
+                status: 500,
+                data: results
+              });
+              return next(err);
+            }
+            //send json to client
+            res.json({
+              response: 'Got a GET query for brand and size',
+              status: 200,
+              data: results
+            });
+          });
     }
 
     //Update the stock levels when a shoe is sold
     var soldUpdate = function(req, res) {
-        let shoe = {
-          id: Number(req.params.id)
+        if (!req.body) {
+          log("No body property found!");
+        } else {
+          //save ID
+          let id = req.params.id;
+
+          res.json({
+            reponse: "Update POST made",
+            shoeID: req.params.id,
+            body: req.body
+          });
         }
-
-        let new_stock = Number(req.params.inStock);
-
-        Models.Shoes.update(shoe, {in_stock: new_stock});
-
-        res.redirect('/api/shoes');
+        //Models.Shoes.update(shoe, {in_stock: new_stock});
+        //res.redirect('/api/shoes');
     }
 
     //Add a new shoe route
