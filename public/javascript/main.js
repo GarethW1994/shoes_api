765af25e4d8d5aeb0823436d45c56f984b154046
GarethW1994 <williams.gareth0@gmail.com>
2017-08-07 11:50:22 +0200

    adding AJAX calls to api for getting all the shoes

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     const log = console.log;
-
-    $.ajax({url: "http://localhost:3107/api/shoes", success: function(result){
-        log(result);
-    }});
+    $.ajax({
+      url: 'http://localhost:3107/api/shoes/',
+      type: 'GET'
+    }).done(function(data){
+      log(data);
+    });
 });
