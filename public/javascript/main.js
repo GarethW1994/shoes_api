4f441eb531212ddc6f5c02ef8f7523af083a7737
GarethW1994 <williams.gareth0@gmail.com>
2017-08-07 11:57:34 +0200

    changed ajax call

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,9 +1,12 @@
+const log = console.log;
+//DOCUMENT READY
 $(document).ready(function(){
-    const log = console.log;
+    //AJAX Call
+    //Retrieving all the data
     $.ajax({
       url: 'http://localhost:3107/api/shoes/',
       type: 'GET'
     }).done(function(data){
       log(data);
-    });
+    });    
 });
