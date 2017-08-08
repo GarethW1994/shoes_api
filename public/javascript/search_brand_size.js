312b24550d4d20048ef90aecfceed325935b9702
GarethW1994 <williams.gareth0@gmail.com>
2017-08-08 11:48:59 +0200

    added ajax call for search by brand and size

--- public/javascript/search_brand_size.js
+++ public/javascript/search_brand_size.js
@@ -1,4 +1,8 @@
-function search_brand_size() {
-
-  return 'hello'
+function search_brand_size(url, brand, size) {
+  $.ajax({
+    url: url + "brand/" + brand + "/size/" + size,
+    type: 'GET'
+  }).done(function(results){
+      log(results);
+  });
 };
