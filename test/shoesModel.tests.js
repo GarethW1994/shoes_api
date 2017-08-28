03d506e9e5ab156f0b112b1e9a3bdbb332861286
GarethW1994 <williams.gareth0@gmail.com>
2017-08-28 12:10:06 +0200

    updated tests

--- test/shoesModel.tests.js
+++ test/shoesModel.tests.js
@@ -15,36 +15,49 @@ describe('store shoes', function() {
     });
 
     it('should add new shoes to MongoDB', function(done) {
-        var newShoe = [{
+        var newShoe = {
             id: 200,
             color: 'Red',
             brand: 'Asics',
             price: 350,
             in_stock: 10,
             size: 3
-        },
-      {
-        id: 200,
-        color: 'Blue',
-        brand: 'Nike',
-        price: 599.99,
-        in_stock: 30,
-        size: 6
-      }]
+        }
 
         Models.Shoes
             .create(newShoe, function(err) {
                 Models.Shoes.find({}, function(err, shoes){
-                  assert.equal(2, shoes.length);
+                  assert.equal(1, shoes.length);
                   done(err);
                 });
             });
     });
 
     it('should return the shoes from MongoDB when queried for a brand', function(done){
-      Models.Shoes.find({brand: 'Asics'}, function(err){
+      Models.Shoes.find({brand: 'Asics'}, function(err), shoes{
           assert.equal(1, shoes.length);
           done(err);
       });
     });
+
+    it('should return the shoes from MongoDB when queried for a size', function(done){
+      Models.Shoes.find({size: 3}, function(err, shoes){
+          assert.equal(1, shoes.length);
+          done(err);
+      });
+    });
+
+    it('should return the shoe from MongoDB when queried for a size and brand', function(done){
+      Models.Shoes.find({brand: 'Asics', size: 3}, function(err){
+          assert.equal(1, shoes.length);
+          done(err);
+      });
+    });
+
+    it('should update the stock of a shoe in MongoDB', function(done) {
+      Models.Shoes.update({id: 200}, {in_stock: 5} function(err, shoes){
+          assert.equal(5, shoes.stock);
+          done(err);
+      });
+    })
 });
