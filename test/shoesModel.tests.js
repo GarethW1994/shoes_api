16ad63e44884cad6a85ae406ec2564e3b342183e
GarethW1994 <williams.gareth0@gmail.com>
2017-08-28 12:04:00 +0200

    updated tests

--- test/shoesModel.tests.js
+++ test/shoesModel.tests.js
@@ -1,11 +1,10 @@
 //require assert
 const assert = require('assert');
 const Model = require('../api/models/shoesModel');
+//connect to MongoDB
+var Models = Model('mongodb://Ninja:$ninja!@ds117093.mlab.com:17093/shoes-api');
 
 describe('store shoes', function() {
-    //connect to MongoDB
-    var Models = Model('mongodb://Ninja:$ninja!@ds117093.mlab.com:17093/shoes-api');
-
    //before it fuction run clear the Database
     beforeEach(function(done) {
         Models.Shoes.find({}, function(err) {
@@ -16,21 +15,36 @@ describe('store shoes', function() {
     });
 
     it('should add new shoes to MongoDB', function(done) {
-        var newShoe = {
+        var newShoe = [{
             id: 200,
-            color: 'red',
+            color: 'Red',
             brand: 'Asics',
             price: 350,
             in_stock: 10,
             size: 3
-        }
+        },
+      {
+        id: 200,
+        color: 'Blue',
+        brand: 'Nike',
+        price: 599.99,
+        in_stock: 30,
+        size: 6
+      }]
 
         Models.Shoes
             .create(newShoe, function(err) {
                 Models.Shoes.find({}, function(err, shoes){
-                  assert.equal(1, shoes.length);
+                  assert.equal(2, shoes.length);
                   done(err);
                 });
             });
+    });
+
+    it('should return the shoes from MongoDB when queried for a brand', function(done){
+      Models.Shoes.find({brand: 'Asics'}, function(err){
+          assert.equal(1, shoes.length);
+          done(err);
+      });
     });
 });
